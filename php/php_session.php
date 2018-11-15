b70797cde0abc3acc40c1c4e8dfb7a28e7250325
DirkVuurst <43130946+DirkVuurst@users.noreply.github.com>
2018-11-15 13:40:32 +0100

    nu helemaal

--- php/php_session.php
+++ php/php_session.php
@@ -14,7 +14,7 @@
             $_SESSION["shopping_cart"][$id] += 1;
         }
         
-        header("Location: ../pages/shopping-cart.html.php");
+        header("Location: ../pages/shopping_cart.html.php");
     } 
     
     // Check if id exists if so, delete item from the shopping cart
@@ -23,6 +23,6 @@
         $delete_id = preg_replace('#[^0-9]#i', '',$_GET['delete_id']);
         $shopping_cart_item[$delete_id] = 1;
         unset($_SESSION["shopping_cart"][$delete_id]); 
-        header("Location: ../pages/shopping-cart.html.php");
+        header("Location: ../pages/shopping_cart.html.php");
     } 
 ?>
